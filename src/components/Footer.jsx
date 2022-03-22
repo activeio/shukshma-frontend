8ec0f0a72910996debf2d14d5bb2d257a59f53b3
Pradeep Suthar <sutharp777@gmail.com>
2022-03-22 22:56:46 +0530

    ui completed :fire: :fire:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,33 @@
 const Footer = (props) => {
     return (
         <footer className="mt-auto text-white-50">
-            <p>
-            Made with ❤️ in India | Copyright © 2022, Shuksma{" "}
+            <p >
+                Made with 💙 in India | team Shuksma <br />
+                developer <a href="https://github.com/sutharp777" className="text-white">
+                    @sutharp777
+                </a>,&nbsp;
+                tester&nbsp;
                 <a href="https://github.com/sutharp777" className="text-white">
-                    @pradeep
+                    @ankit
+                </a>,&nbsp;
+                report&nbsp;
+                <a href="https://github.com/sutharp777" className="text-white">
+                    @manan
+                </a>, and&nbsp;
+                cloud&nbsp;
+                <a href="https://github.com/sutharp777" className="text-white">
+                    @rohit
                 </a>
+                <hr />
             </p>
+            <ul class="nav justify-content-between pb-3 mb-3">
+                {/* <li class="nav-item"><img height="33" src="./express.png"/></li> */}
+                <li class="nav-item"><img height="35" src="./node.png" /></li>
+                <li class="nav-item"><img height="35" src="./react.png" /></li>
+                <li class="nav-item"><img height="35" src="./redux.png" /></li>
+                <li class="nav-item"><img height="35" src="./bootstrap.png" /></li>
+                <li class="nav-item"><img height="35" src="./deocean.png" /></li>
+            </ul>
         </footer>
     );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,139 @@
 import "./Home.css"
 import { Component } from "react"
-var QRCode = require('qrcode')
+import axios from "axios"
+import ShareLink from "./ShareLink"
+import Notification from './Notification';
 
 class Home extends Component {
+    state = {
+        loading: false,
+        url: '',
+        key: '',
+        msg: '',
+        msgType: '',
+        shortLink: '',
+        shareLink: null,
+        email: ''
+    }
+    slugPattern = RegExp('^[-\\w]*$')
 
-    createQRCode() {
+    uriPattern = RegExp('^(https?:\\/\\/)?(www.)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$', 'i') // fragment locator
+
+    getLoadingState() {
+        return (<>
+            <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+            &nbsp;Loading...
+        </>)
+    }
+    init = () => {
+        axios.get("/api/current_user")
+            .then((res) => {
+                this.setState({ email: res.data.email })
+                localStorage.setItem('email', this.state.email)
+            })
+
+    }
+    componentDidMount() {
+        const uri = document.getElementById("exampleFormControlInput1")
+        uri.focus()
+        this.init()
+    }
+
+    handleUrlChange = (event) => {
+        this.setState({ url: event.target.value });
+    }
+    handleKeyChange = (event) => {
+        this.setState({ key: event.target.value });
+    }
+
+    shortItCalled = () => {
+        this.setState({
+            loading: true,
+            msg: '',
+            shareLink: ''
+        })
+        const uri = document.getElementById("exampleFormControlInput1")
+        const slug = document.getElementById("urlSlug")
+
+        if (!this.uriPattern.test(this.state.url.trim())) {
+            uri.focus()
+            uri.classList.add("apply-shake")
+            setTimeout(() => uri.classList.remove("apply-shake"), 1000)
+            this.setState({ loading: false })
+            return
+        } else if (!this.slugPattern.test(slug.value.trim())) {
+            slug.focus()
+            slug.classList.add("apply-shake")
+            setTimeout(() => slug.classList.remove("apply-shake"), 1000)
+            this.setState({ loading: false })
+            return
+        }
+        const url = 'http://localhost:5000/';
+
+        this.postLink(url)
+
+    }
+
+    genrateQRCode = (url) => {
+        var QRCode = require('qrcode')
         var canvas = document.getElementById('canvas')
-        var slug = document.getElementById('urlSlug').innerHTML
 
-        QRCode.toCanvas(canvas, `http://localhost:4000/${slug}`, function (error) {
+        QRCode.toCanvas(document.getElementById('canvas'), url, function (error) {
             if (error) console.error(error)
             console.log('success!');
         })
+
     }
+    postLink = (url) => {
+        const timeStamp = setTimeout(() => {
+            this.setState({
+                'shareLink': null,
+                loading: false,
+                msg: 'unable to connect server',
+                msgType: 'danger'
+            })
+        }, 10000);
+
+        axios.post(url, {
+            key: this.state.key,
+            link: this.state.url,
+            email: this.state.email
+        }).then((response) => {
+            const data = response.data
+            const key = data._doc ? data._doc.key : '';
+            const url = `localhost:5000/${key}`
+            this.setState({
+                'shareLink': key ? (<ShareLink url={url} _key={`/${key}`} />) : null,
+                loading: false,
+                msg: response.data.msg,
+                msgType: response.data.status
+            })
+            this.genrateQRCode(url)
+            clearTimeout(timeStamp);
+        }).catch((err) => {
+            this.setState({
+                'shareLink': null,
+                loading: false,
+                msg: 'unable to connect server',
+                msgType: 'danger'
+            })
+        })
+
+    }
+
     render() {
         return (
             <main className="px-3">
-                <h1>Link Manager</h1>
+                {this.state.loading ?
+                    ""
+                    : <Notification msg={this.state.msg} msgType={this.state.msgType} />
+                }
+                <h2>Link Manager</h2>
                 <p className="lead">
                     Short, Share, Track it!
                 </p>
@@ -24,6 +141,8 @@ class Home extends Component {
                 <div className="mb-3">
                     <input
                         type="text"
+                        value={this.state.url}
+                        onChange={this.handleUrlChange}
                         className="form-control-lg"
                         id="exampleFormControlInput1"
                         placeholder="https://github.com/..." />
@@ -32,24 +151,33 @@ class Home extends Component {
                 <div className="mb-3">
                     <input
                         type="text"
+                        value={this.state.key}
+                        onChange={this.handleKeyChange}
                         className="form-control-lg"
                         id="urlSlug"
                         placeholder="slug" />
                 </div>
-
+                <br />
+                <span className='lead'>
+                    {this.state.shareLink || this.state.msg}
+                </span>
+                <br />
                 <p className="lead">
-                    <a
+                    <button
                         href="#"
                         className="btn btn-lg btn-secondary fw-bold border-white bg-white text-dark"
-                        onClick={this.createQRCode}
-                    >
-                        Short it!
-                    </a>
+                        disabled={this.state.loading}
+                        onClick={this.shortItCalled}>
+                        {this.state.loading ?
+                            this.getLoadingState()
+                            : <span>&nbsp;&nbsp;&nbsp;Short it!&nbsp;&nbsp;&nbsp;&nbsp;</span>}
+                    </button>
                 </p>
 
                 <div className="text-center">
                     <canvas id="canvas" className="rounded"></canvas>
                 </div>
+
 
             </main>
         );
